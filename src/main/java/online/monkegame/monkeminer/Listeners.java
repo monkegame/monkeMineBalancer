a3dc137d032d7bba8eafa80f0794da73a1f03170
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-08-07 20:06:07 +0200

    add check for silk touch

--- src/main/java/online/monkegame/monkeminer/Listeners.java
+++ src/main/java/online/monkegame/monkeminer/Listeners.java
@@ -30,26 +30,26 @@ public class Listeners implements Listener {
     public ColorGenerator cg;
     public List<ItemStack> itemlist = new ArrayList<>();
     public Map<Material, Integer> materials = Map.ofEntries(
-            entry(Material.COAL_ORE,1),
-            entry(Material.DEEPSLATE_COAL_ORE,1),
-            entry(Material.DEEPSLATE_DIAMOND_ORE,5),
-            entry(Material.DEEPSLATE_EMERALD_ORE,4),
-            entry(Material.DEEPSLATE_GOLD_ORE,3),
-            entry(Material.DEEPSLATE_COPPER_ORE,2),
-            entry(Material.DEEPSLATE_IRON_ORE,2),
-            entry(Material.DEEPSLATE_LAPIS_ORE,3),
-            entry(Material.DEEPSLATE_REDSTONE_ORE,3),
-            entry(Material.COPPER_ORE,2),
-            entry(Material.DIAMOND_ORE,5),
-            entry(Material.EMERALD_ORE,4),
-            entry(Material.GOLD_ORE,3),
+            entry(Material.COAL_ORE,0),
+            entry(Material.DEEPSLATE_COAL_ORE,0),
+            entry(Material.DEEPSLATE_DIAMOND_ORE,4),
+            entry(Material.DEEPSLATE_EMERALD_ORE,3),
+            entry(Material.DEEPSLATE_GOLD_ORE,1),
+            entry(Material.DEEPSLATE_COPPER_ORE,1),
+            entry(Material.DEEPSLATE_IRON_ORE,1),
+            entry(Material.DEEPSLATE_LAPIS_ORE,2),
+            entry(Material.DEEPSLATE_REDSTONE_ORE,2),
+            entry(Material.COPPER_ORE,1),
+            entry(Material.DIAMOND_ORE,4),
+            entry(Material.EMERALD_ORE,3),
+            entry(Material.GOLD_ORE,2),
             entry(Material.NETHER_GOLD_ORE,1),
-            entry(Material.NETHER_QUARTZ_ORE,2),
-            entry(Material.REDSTONE_ORE,3),
-            entry(Material.IRON_ORE,2),
-            entry(Material.LAPIS_ORE,3),
-            entry(Material.AMETHYST_CLUSTER,3),
-            entry(Material.ANCIENT_DEBRIS, 6)
+            entry(Material.NETHER_QUARTZ_ORE,1),
+            entry(Material.REDSTONE_ORE,2),
+            entry(Material.IRON_ORE,1),
+            entry(Material.LAPIS_ORE,2),
+            entry(Material.AMETHYST_CLUSTER,2),
+            entry(Material.ANCIENT_DEBRIS, 5)
     );
 
     public Listeners(Main main, Logger logger) {
@@ -71,13 +71,13 @@ public class Listeners implements Listener {
     }
 
     public void dropRate(BlockBreakEvent blockbreak) {
-        if (materials.containsKey(blockbreak.getBlock().getBlockData().getMaterial())) {
+        if (materials.containsKey(blockbreak.getBlock().getBlockData().getMaterial()) && !blockbreak.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
             double weewoo = Math.random();
-            weewoo = weewoo + (materials.get(blockbreak.getBlock().getBlockData().getMaterial()) * 0.1);
+            weewoo = weewoo + (materials.get(blockbreak.getBlock().getBlockData().getMaterial()) * 0.09);
             if (weewoo < 0.931) {
                 //92.1% chance of single drops
                 itemlist.addAll(blockbreak.getBlock().getDrops());
-            } else if (weewoo >= 0.931  && weewoo < 0.99) {
+            } else if (weewoo >= 0.931 && weewoo < 0.99) {
                 //6.9% chance of double drops - Smertieboi
                 itemlist.addAll(blockbreak.getBlock().getDrops());
                 itemlist.addAll(blockbreak.getBlock().getDrops());
@@ -88,6 +88,9 @@ public class Listeners implements Listener {
                 itemlist.addAll(blockbreak.getBlock().getDrops());
                 itemlist.addAll(blockbreak.getBlock().getDrops());
             }
+        } else if (blockbreak.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
+            // just add the block normally if the player has a silk touch tool
+            itemlist.add(new ItemStack(blockbreak.getBlock().getBlockData().getMaterial()));
         } else {
             itemlist.addAll(blockbreak.getBlock().getDrops());
         }
