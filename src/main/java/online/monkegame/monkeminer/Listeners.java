ce885022a6bcd3fcbdf3bedb463fb04bb0b6b113
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-08-07 18:38:52 +0200

    add check if it's an ore block

--- src/main/java/online/monkegame/monkeminer/Listeners.java
+++ src/main/java/online/monkegame/monkeminer/Listeners.java
@@ -1,6 +1,7 @@
 package online.monkegame.monkeminer;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +9,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -16,6 +18,27 @@ public class Listeners implements Listener {
     public Main plugin;
     public Logger log;
     public List<ItemStack> itemlist = new ArrayList<>();
+    public List<Material> materials = new ArrayList<>(Arrays.asList(
+            Material.COAL_ORE,
+            Material.DEEPSLATE_COAL_ORE,
+            Material.DEEPSLATE_DIAMOND_ORE,
+            Material.DEEPSLATE_EMERALD_ORE,
+            Material.DEEPSLATE_GOLD_ORE,
+            Material.DEEPSLATE_COPPER_ORE,
+            Material.DEEPSLATE_IRON_ORE,
+            Material.DEEPSLATE_LAPIS_ORE,
+            Material.DEEPSLATE_REDSTONE_ORE,
+            Material.COPPER_ORE,
+            Material.DIAMOND_ORE,
+            Material.EMERALD_ORE,
+            Material.GOLD_ORE,
+            Material.NETHER_GOLD_ORE,
+            Material.NETHER_QUARTZ_ORE,
+            Material.REDSTONE_ORE,
+            Material.IRON_ORE,
+            Material.LAPIS_ORE,
+            Material.AMETHYST_CLUSTER
+    ));
 
     public Listeners(Main main, Logger logger) {
         this.plugin = main;
@@ -35,18 +58,23 @@ public class Listeners implements Listener {
     }
 
     public void dropRate(BlockBreakEvent blockbreak) {
-        if (blockbreak.getPlayer().getLevel() > 10) {
+        if (blockbreak.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS) && materials.contains(blockbreak.getBlock().getBlockData().getMaterial())) {
             double weewoo = Math.random();
-            if (weewoo < 0.65) {
+            if (weewoo <= 0.921) {
+                //92.1% chance of single drops
                 itemlist.addAll(blockbreak.getBlock().getDrops());
-            } else if (weewoo > 0.75) {
+            } else if (weewoo >= 0.931) {
+                //6.9% chance of double drops - Smertieboi
                 itemlist.addAll(blockbreak.getBlock().getDrops());
                 itemlist.addAll(blockbreak.getBlock().getDrops());
             } else {
+                //1% chance of triple drops
                 itemlist.addAll(blockbreak.getBlock().getDrops());
                 itemlist.addAll(blockbreak.getBlock().getDrops());
                 itemlist.addAll(blockbreak.getBlock().getDrops());
             }
+        } else {
+            itemlist.addAll(blockbreak.getBlock().getDrops());
         }
     }
 
